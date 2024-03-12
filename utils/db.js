94f4c54816744634c923ea402a2ecc9d6ec04d75
Ripon-Mardy <mardyripon10@gmail.com>
2024-03-12 20:45:21 +0600

    Your commit message here

--- utils/db.js
+++ utils/db.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose"
 const database = async () => {
     try {
         const mongouri = process.env.MONGO_URL;
-        await mongoose.connect(mongouri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect(mongouri)
         console.log('mongodb connection succesfull');
     } catch (error) {
         throw new Error('Error in connecting mongodb..')
