84d60249cf6387ee07be5e042c1545564787a43a
Ripon-Mardy <mardyripon10@gmail.com>
2024-03-28 01:38:16 +0600

    change2

--- db/Db.js
+++ db/Db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const database = async () => {
     try {
-        const dbUrl = process.env.DATABASE_URL;
-        await mongoose.createConnection(dbUrl, {
+        // const dbUrl = process.env.DATABASE_URL;
+        await mongoose.createConnection("mongodb+srv://riponmardy:Ripon&m1@cluster0.h1xsxyh.mongodb.net/Faq?retryWrites=true&w=majority&appName=Cluster0", {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
